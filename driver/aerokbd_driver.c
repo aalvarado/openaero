726360bfa5bfb2d5eae9154e497835e147fe5958
Hemanth Bollamreddi <blmhemu@gmail.com>
2019-10-23 17:13:21 +0530

    Added support for Aero15SA model.

--- driver/aerokbd_driver.c
+++ driver/aerokbd_driver.c
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * HID driver for Chu Yuen Keyboard
+ * HID driver for Gigabyte Aero Keyboards
  * Copyright (c) 2019 Hemanth Bollamreddi
 */
 
 #include <linux/hid.h>
 #include <linux/module.h>
+#include "aerokbd_driver.h"
 
 MODULE_AUTHOR("Hemanth Bollamreddi <blmhemu@gmail.com>");
-MODULE_DESCRIPTION("HID Keyboard driver for Gigabyte Aero Keyboard.");
+MODULE_DESCRIPTION("HID Keyboard driver for Gigabyte Aero Keyboards.");
 MODULE_LICENSE("GPL v2");
 
 //TODO: If put in mainstream kernel, modify this file to include the VID and PID.
 //#include "hid-ids.h"
-#define USB_VENDOR_ID_CHUYUEN 0x1044
-#define USB_DEVICE_ID_CHUYUEN_HIDKBD 0x7A39
 
 #define HIDRAW_FN_ESC 0x04000084
 #define HIDRAW_FN_F2 0x0400007C
@@ -27,12 +26,12 @@ MODULE_LICENSE("GPL v2");
 
 #define make_u32(a, b, c, d) a << 24 | b << 16 | c << 8 | d
 
-static int chuyuen_kbd_raw_event(struct hid_device *hdev, struct hid_report *report, u8 *rd, int size)
+static int aero_kbd_raw_event(struct hid_device *hdev, struct hid_report *report, u8 *rd, int size)
 {
 	if (report->id == 4 && size == 4)
 	{
 		u32 hidraw = make_u32(rd[0], rd[1], rd[2], rd[3]);
-		// printk("Chu Yuen Raw Event. hidraw code : %x", hidraw);
+		// printk("Aero kbd raw event. hidraw code : %x", hidraw);
 		switch (hidraw)
 		{
 		case HIDRAW_FN_F3:
@@ -53,9 +52,9 @@ static int chuyuen_kbd_raw_event(struct hid_device *hdev, struct hid_report *rep
 	return 0;
 }
 
-static int chuyuen_probe(struct hid_device *hdev, const struct hid_device_id *id)
+static int aero_kbd_probe(struct hid_device *hdev, const struct hid_device_id *id)
 {
-	// printk("Chu Yuen Driver Loaded.");
+	// printk("Aero kbd driver loaded.");
 	int ret;
 	hdev->quirks |= HID_QUIRK_INPUT_PER_APP;
 
@@ -66,15 +65,16 @@ static int chuyuen_probe(struct hid_device *hdev, const struct hid_device_id *id
 	return hid_hw_start(hdev, HID_CONNECT_DEFAULT);
 }
 
-static const struct hid_device_id chuyuen_kbd_devices[] = {
-	{HID_USB_DEVICE(USB_VENDOR_ID_CHUYUEN, USB_DEVICE_ID_CHUYUEN_HIDKBD)},
+static const struct hid_device_id aero_kbd_devices[] = {
+	{HID_USB_DEVICE(USB_VENDOR_ID_AERO15XV8, USB_DEVICE_ID_AERO15Xv8)},
+	{HID_USB_DEVICE(USB_VENDOR_ID_AERO15SA, USB_DEVICE_ID_AERO15SA)},
 	{}
 };
-MODULE_DEVICE_TABLE(hid, chuyuen_kbd_devices);
+MODULE_DEVICE_TABLE(hid, aero_kbd_devices);
 
-static struct hid_driver chuyuen_kbd_driver = {
-	.name = "Chu Yuen",
-	.id_table = chuyuen_kbd_devices,
-	.probe = chuyuen_probe,
-	.raw_event = chuyuen_kbd_raw_event};
-module_hid_driver(chuyuen_kbd_driver);
+static struct hid_driver aero_kbd_driver = {
+	.name = "aerokbd",
+	.id_table = aero_kbd_devices,
+	.probe = aero_kbd_probe,
+	.raw_event = aero_kbd_raw_event};
+module_hid_driver(aero_kbd_driver);
